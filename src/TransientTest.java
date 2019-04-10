5cde76c44736cdabcde7cf36b501ce0d0441460b
zigar <zigar.qin@oracle.com>
2019-04-10 15:21:14 +0800

    add demo for Externalizable interface usage

--- src/TransientTest.java
+++ src/TransientTest.java
@@ -3,42 +3,48 @@ import java.io.*;
 public class TransientTest {
 
     public static void main(String[] args) {
+        ExternalizableTest et = new ExternalizableTest();
         User user = new User();
         user.setUsername("Zigar");
         user.setPasswd("123456");
-
+        user.setAge((short)30);
         System.out.println("read before Serializable: ");
         System.out.println("username: " + user.getUsername());
-        System.err.println("password: " + user.getPasswd());
+        System.out.println("password: " + user.getPasswd());
+        System.out.println("age: " + user.getAge());
 
         try {
-            ObjectOutputStream os = new ObjectOutputStream(
-                    new FileOutputStream("/tmp/user.txt"));
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("/tmp/user.txt"));
             os.writeObject(user); // 将User对象写进文件
             os.flush();
             os.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            ObjectOutput out = new ObjectOutputStream(new FileOutputStream(new File("/tmp/test")));
+            out.writeObject(et);
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-            ObjectInputStream is = new ObjectInputStream(new FileInputStream(
-                    "/tmp/user.txt"));
+            //在反序列化之前改变age的值
+            User.age = (short)40;
+
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("/tmp/user.txt"));
             user = (User) is.readObject(); // 从流中读取User的数据
             is.close();
 
             System.out.println("\nread after Serializable: ");
             System.out.println("username: " + user.getUsername());
             System.err.println("password: " + user.getPasswd());
+            System.err.println("age: " + user.getAge() + " //一个静态变量不管是否被transient修饰，均不能被序列化");
+            ObjectInput in = new ObjectInputStream(new FileInputStream(new File("/tmp/test")));
+            ExternalizableTest et_in = (ExternalizableTest) in.readObject();
+            System.out.println(et_in.content);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 
 }
@@ -49,6 +55,17 @@ class User implements Serializable{
 
     private String username;
     private transient String passwd;
+    static short age;
+
+    Short getAge() {
+        return age;
+    }
+
+    void setAge(Short age) {
+        this.age = age;
+    }
+
+
 
     public String getUsername() {
         return username;
@@ -64,5 +81,26 @@ class User implements Serializable{
 
     public void setPasswd(String passwd) {
         this.passwd = passwd;
+    }
+}
+
+class ExternalizableTest implements Externalizable {
+    private static final long serialVersionUID = -2513747641863637393L;
+
+    public transient String content = "Content: Yes, I'll always be serialized whether or not I'm transient";
+
+    public ExternalizableTest(){
+
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(content);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException,
+            ClassNotFoundException {
+        content = (String) in.readObject();
     }
 }
